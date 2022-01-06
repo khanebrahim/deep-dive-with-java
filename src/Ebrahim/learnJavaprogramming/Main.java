987910486cbad0bb3cbdd7f090d452877a0c7964
ebrahim <ebrahimkhan5222@gmail.com>
2022-01-06 21:02:08 +0600

    primitive dataTypes

---
+++ src/Ebrahim/learnJavaprogramming/Main.java
@@ -0,0 +1,16 @@
+package Ebrahim.learnJavaprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello java");
+        int myValue = 8885;
+        int myMaxValue =Integer.MAX_VALUE;
+        int myMinValue =Integer.MIN_VALUE;
+        System.out.println("Int myMaxValue = " + myMaxValue);
+        System.out.println("Int myMinValue = " + myMinValue);
+        System.out.println("myValue");
+
+    }
+}
