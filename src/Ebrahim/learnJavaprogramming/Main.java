b5ce8b4ba5b5fc5b87bfbf2725a1d61bb74bc623
ebrahim <ebrahimkhan5222@gmail.com>
2022-01-06 21:09:03 +0600

    primitive dataTypes

--- src/Ebrahim/learnJavaprogramming/Main.java
+++ src/Ebrahim/learnJavaprogramming/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("Int myMaxValue = " + myMaxValue);
         System.out.println("Int myMinValue = " + myMinValue);
         System.out.println("myValue");
+        System.out.println("Busted max value = " + (myMaxValue + 2));
 
     }
 }
